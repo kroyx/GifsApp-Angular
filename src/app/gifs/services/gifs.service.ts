d80d1094549a972ae0d04f557e7a74a558b943b6
Lelei <66555957+xLelei@users.noreply.github.com>
2023-04-23 23:31:46 +0200

    Integrado GIPHY para la obtencion de los gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,52 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Gif, SearchResponse} from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  public gifsList: Gif[] = [];
   private tagsHistory: string[] = [];
+  private apiKey: string = 'dk5ta3KUn67iZnH3taPlcQ4LTIOqDfxj';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get getTagsHistory(): string[] {
     return [...this.tagsHistory];
   }
+
+  public searchTag(tag: string): void {
+    if (tag === '') return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+        .subscribe(resp => {
+          this.gifsList = resp.data;
+        });
+  }
+
+  // public async searchTag(tag: string): Promise<void> {
+  //   // Se valida que se haya introducido algo
+  //   if (tag === '') return;
+  //   this.organizeHistory(tag);
+  //
+  //   // Opcion 1
+  //   // fetch('https://api.giphy.com/v1/gifs/search?api_key=dk5ta3KUn67iZnH3taPlcQ4LTIOqDfxj&q=valorant&limit=10')
+  //   //   .then(resp => resp.json())
+  //   //   .then(data => console.log(data));
+  //
+  //   // Opcion 2
+  //   const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=dk5ta3KUn67iZnH3taPlcQ4LTIOqDfxj&q=valorant&limit=10');
+  //   const data = await resp.json();
+  //   console.log(data);
+  // }
 
   private organizeHistory(tag: string): void {
     tag = tag.toLowerCase();
@@ -26,11 +61,5 @@ export class GifsService {
     if (this.tagsHistory.length > 10) {
       this.tagsHistory.pop();
     }
-  }
-
-  public searchTag(tag: string): void {
-    // Se valida que se haya introducido algo
-    if (tag === '') return;
-    this.organizeHistory(tag);
   }
 }
