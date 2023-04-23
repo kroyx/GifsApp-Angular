df835df04bb90b116caa6f4122e5043976fa074b
Lelei <66555957+xLelei@users.noreply.github.com>
2023-04-23 22:42:19 +0200

    Finalizada la funcionalidad del historial de busquedas

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,24 @@ export class GifsService {
     return [...this.tagsHistory];
   }
 
-  public searchTag(tag: string): void {
+  private organizeHistory(tag: string): void {
+    tag = tag.toLowerCase();
+
+    if (this.tagsHistory.includes(tag)) {
+      this.tagsHistory = this.tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
     this.tagsHistory.unshift(tag);
+
+    // Se restringe el historial a 10 elementos
+    if (this.tagsHistory.length > 10) {
+      this.tagsHistory.pop();
+    }
+  }
+
+  public searchTag(tag: string): void {
+    // Se valida que se haya introducido algo
+    if (tag === '') return;
+    this.organizeHistory(tag);
   }
 }
