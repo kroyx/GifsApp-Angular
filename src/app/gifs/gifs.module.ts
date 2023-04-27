e2ff93f56d108f324c15b8a6f2f154ba78da74bf
Lelei <66555957+xLelei@users.noreply.github.com>
2023-04-27 20:06:30 +0200

    Finalizado el desarrollo de la aplicacion

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,6 +3,8 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import {SearchBoxComponent} from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import {SharedModule} from '../shared/shared.module';
 
 
 
@@ -10,14 +12,16 @@ import { CardListComponent } from './components/card-list/card-list.component';
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
+    CardListComponent,
+    GifsCardComponent
   ],
   exports: [
     HomePageComponent
   ],
   imports: [
     CommonModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    SharedModule
   ]
 })
 export class GifsModule { }
---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styles: []
+})
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public isLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw  new Error('URL is required');
+  }
+
+  onLoad() {
+    setTimeout(() => {
+      this.isLoaded = true;
+    }, 1000)
+  }
+}
